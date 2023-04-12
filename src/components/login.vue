fd43f84ca72a5a12bfa38475336b0b611ea5d053
renlixiang <2656742989@qq.com>
2023-04-12 17:20:34 +0800

    登录页基本功能完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,17 +1,16 @@
 <template>
     <div class="body">
         <!--登录页-->
-        <transition name="login">
-        <div class="container" v-show= "Flag.login">
+        <div class="container" v-show= "this.flag.login">
             <div class="block">
-                <img class="headImg" :src="massage.img" alt="">
+                <img class="headImg" :src="message.img" alt="">
             </div>
             <div class="user">
-                <el-input v-model="massage.inputUser" placeholder="请输入您的账号" clearable />
+                <el-input v-model="message.inputUser" placeholder="请输入您的账号" clearable />
             </div>
             <div class="password">
                 <el-input
-                    v-model="massage.inputPassword"
+                    v-model="message.inputPassword"
                     type="password"
                     placeholder="请输入您的密码"
                     show-password
@@ -22,25 +21,23 @@
                 <el-link class="register" :underline="false" @click="skipRegistry">没有账号？去注册</el-link>
             </div>
             <div class="button">
-                <el-button class="btt" type="primary">登录</el-button>
+                <el-button class="btt" type="primary" @click="login">登录</el-button>
             </div>
         </div>
-        </transition>
         <!--注册页-->
-        <transition name="registry">
-        <div class="container" v-show="Flag.registry">
+        <div class="container" v-show="this.flag.registry">
             <div class="first-registry">
-                <el-input v-model="massage.phone" placeholder="请输入您的手机号" clearable />
+                <el-input v-model="message.phone" placeholder="请输入您的手机号" clearable />
             </div>
             <div class="registry">
-                <el-input v-model="massage.mail" placeholder="请输入您的邮箱" clearable />
+                <el-input v-model="message.email" placeholder="请输入您的邮箱" clearable />
             </div>
             <div class="registry">
-                <el-input v-model="massage.name" placeholder="请输入您的姓名" clearable />
+                <el-input v-model="message.userName" placeholder="请输入您的账号" clearable />
             </div>
             <div class="registry">
                 <el-date-picker
-                    v-model="massage.birthday"
+                    v-model="message.birthday"
                     type="date"
                     placeholder="请选择您的生日"
                     size="large"
@@ -49,14 +46,14 @@
             </div>
             <div class="registry">
                 <el-input
-                    v-model="massage.newPassword"
+                    v-model="message.newPassword"
                     type="password"
                     placeholder="请设置您的密码"
                 />
             </div>
             <div class="registry">
                 <el-input
-                    v-model="massage.confirmPassword"
+                    v-model="message.confirmPassword"
                     type="password"
                     placeholder="请再次确认您的密码"
                     show-password
@@ -66,87 +63,186 @@
                 <el-link class="register" :underline="false" @click="skipLogin">已有账号，去登录</el-link>
             </div>
             <div class="button">
-                <el-button class="btt" type="primary">确认</el-button>
+                <el-button class="btt" type="primary" @click="register">确认</el-button>
             </div>
         </div>
-        </transition>
+
         <!--忘记密码页-->
-        <transition name="forget">
-        <div class="container" v-show="Flag.forget">
+        <div class="container" v-show="this.flag.forget">
             <div class="skip">
                 <el-link class="cancel" :underline="false" @click="skipLogin">取消</el-link>
             </div>
             <div class="first-forget">
-                <el-input v-model="massage.inputUser" placeholder="请输入您的账号" clearable />
+                <el-input v-model="message.userName" placeholder="请输入您的账号" clearable />
             </div>
             <div class="setPassword">
-                <el-input v-model="massage.phone" placeholder="请输入您的手机号" clearable />
+                <el-input v-model="message.phone" placeholder="请输入您的手机号" clearable />
             </div>
             <div class="setPassword">
-                <el-input v-model="massage.name" placeholder="请输入您的姓名" clearable />
+                <el-input v-model="message.email" placeholder="请输入您的邮箱" clearable />
             </div>
             <div class="setPassword">
                 <el-input
-                    v-model="massage.newPassword"
+                    v-model="message.newPassword"
                     type="password"
                     placeholder="请设置您的新密码"
                 />
             </div>
             <div class="setPassword">
                 <el-input
-                    v-model="massage.confirmPassword"
+                    v-model="message.confirmPassword"
                     type="password"
                     placeholder="请再次确认您的密码"
                     show-password
                 />
             </div>
             <div class="button">
-                <el-button class="btt" type="primary">确认</el-button>
+                <el-button class="btt" type="primary" @click="forgetPassword">确认</el-button>
             </div>
         </div>
-        </transition>
     </div>
 </template>
 
 <script lang="js">
 export default {
     name: "loginVue",
+    mounted() {
+
+    },
     data() {
         return {
-            massage : {
+            message : {
                 inputUser: '',
                 inputPassword: '',
                 img: require("../assets/logo.png"),
+                userName: '',
                 birthday: '',
                 phone: '',
-                mail: '',
-                name:'',
+                email: '',
                 newPassword:'',
                 confirmPassword:'',
                 },
-            Flag : {
+            flag : {
                 login: true,
                 forget: false,
                 registry: false
              },
+            users: this.$store.state.users,
+            data: ''
         }},
     methods: {
         skipRegistry() {
-            this.Flag.login = false
-            this.Flag.forget = false
-            this.Flag.registry = true
+            this.flag.login = false
+            this.flag.forget = false
+            this.flag.registry = true
         },
 
         skipForgetPassword () {
-            this.Flag.login = false
-            this.Flag.registry = false
-            this.Flag.forget = true
+            this.flag.login = false
+            this.flag.registry = false
+            this.flag.forget = true
         },
 
         skipLogin () {
-            this.Flag.registry = false
-            this.Flag.forget = false
-            this.Flag.login = true
+            this.flag.registry = false
+            this.flag.forget = false
+            this.flag.login = true
+        },
+
+        // 用户注册
+        register (){
+            // let phoneReg = /^1[345789][0-9]{9}$/
+            // // 密码包括数字，字母，长度8-16
+            // let passwordReg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$/
+            // let emailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
+
+            // if( !phoneReg.test(this.phone) ) {
+            //     this.$message({
+            //         type:"error",
+            //         message: '手机号格式错误，请重新输入',
+            //         duration: 2000
+            //     })
+            // }else if(!emailReg.test(this.email)) {
+            //     this.$message({
+            //         type:"error",
+            //         message: '邮箱格式错误，请重新输入',
+            //         duration: 2000
+            //     })
+            // }else if(!passwordReg.test(this.newPassword)) {
+            //     this.$message({
+            //         type:"error",
+            //         message: '密码不符合要求，请重新设置',
+            //         duration: 2000
+            //     })
+            // }else if(!(this.newPassword === this.confirmPassword)) {
+            //     this.$message({
+            //         type:"error",
+            //         message: '两次密码不一样，请重新输入',
+            //         duration: 2000
+            //     })
+            // } else {
+            //     let data = {
+            //         userName: this.message.userName,
+            //         password: this.message.newPassword,
+            //         phone: this.message.phone,
+            //         birthday: this.message.birthday,
+            //         email: this.message.email
+            //     }
+            //     this.$store.commit("add_user", data)
+            //     this.$message({
+            //         type:"success",
+            //         message: '注册成功，请登录',
+            //         duration: 2000
+            //     })
+            //     this.skipLogin()
+            // }
+
+            this.data = {
+                userName: this.message.userName,
+                password: this.message.newPassword,
+                phone: this.message.phone,
+                birthday: this.message.birthday,
+                email: this.message.email
+            }
+            this.$store.commit("add_user", this.data)
+            this.$message({
+                type:"success",
+                message: '注册成功，请登录',
+                duration: 2000
+            })
+            this.skipLogin()
+
+        },
+
+        // 用户登录
+        login () {
+            console.log(this.users)
+            for (let i in this.users) {
+                if ((this.users[i].userName === this.message.inputUser) && (this.users[i].password === this.message.inputPassword)) {
+                    this.$message({
+                        type: 'success',
+                        message: '登录成功',
+                        duration: 2000
+                    })
+                }
+            }
+        },
+
+        forgetPassword () {
+            this.data = {
+                userName: this.message.userName,
+                password: this.message.newPassword,
+                phone: this.message.phone,
+                birthday: this.message.birthday,
+                email: this.message.email
+            }
+            this.$store.commit("updatePassword", this.data)
+            this.$message({
+                type:"success",
+                message: '密码设置成功，请登录',
+                duration: 2000
+            })
+            this.skipLogin()
         }
 
     }
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-
+    users: [{
+        userName: 'admin',
+        password: '123456',
+        phone: '19919878143',
+        birthday: '1999-12-01',
+        email: '2656742989@qq.com'
+    }]
 }
 
 const actions = {
@@ -11,14 +17,30 @@ const actions = {
 }
 
 const mutations = {
+    add_user: function (state, data) {
+        state.users.push(data)
+    },
+
+    updatePassword: function (state, data) {
+        for (let i in state.users) {
+            if((data.userName === state.users[i].userName) && (data.phone === state.users[i].phone) && (data.email === state.users[i].email)) {
+                state.users[i].password = data.password
+            }
+        }
+    }
+}
+
+const getters = {
 
 }
+
 
 const store = new Vuex.Store({
     namespaced: true,
     state,
     actions,
-    mutations
+    mutations,
+    getters
 
 })
 export default store
